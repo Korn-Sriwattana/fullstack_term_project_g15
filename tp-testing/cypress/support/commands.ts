dc0314fc14b0e97794eb77e3079b01301b04c66b
Yada <yada_pholam@cmu.ac.th>
2025-10-14 01:07:05 +0700

    update cypress testing: home likedSongs playlists

--- tp-testing/cypress/support/commands.ts
+++ tp-testing/cypress/support/commands.ts
@@ -26,37 +26,3 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
 // cypress/support/commands.ts
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      saveLocalStorage(): Chainable<void>;
-      restoreLocalStorage(): Chainable<void>;
-    }
-  }
-}
-
-Cypress.Commands.add('saveLocalStorage', () => {
-  cy.window().then((win) => {
-    const localStorage: { [key: string]: string } = {};
-    for (let i = 0; i < win.localStorage.length; i++) {
-      const key = win.localStorage.key(i);
-      if (key) {
-        localStorage[key] = win.localStorage.getItem(key) || '';
-      }
-    }
-    cy.wrap(localStorage).as('localStorage');
-  });
-});
-
-Cypress.Commands.add('restoreLocalStorage', () => {
-  cy.get<{ [key: string]: string }>('@localStorage').then((localStorage) => {
-    cy.window().then((win) => {
-      Object.entries(localStorage).forEach(([key, value]) => {
-        // win.localStorage.setItem(key, value);
-      });
-    });
-  });
-});
-
-export {};
